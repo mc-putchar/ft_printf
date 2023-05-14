4194bf03d0c013297c3912a8acefce272380b3ca
Martin Cutura <mcutura@student.42berlin.de>
2023-05-14 11:18:26 +0200

    Added full bonus functionality

--- ft_printf_converters.c
+++ ft_printf_converters.c
@@ -14,37 +14,76 @@
 
 char	*ft_printf_int(t_format *fmt)
 {
-	char	*str;
+	char	*pad;
+	char	*tmp;
+	int		len;
 
-	str = ft_itoa(fmt->u_arg.i);
-	return (str);
+	fmt->out = ft_itoa(fmt->u_arg.i);
+	len = fmt->prec - ft_strlen(fmt->out);
+	if (fmt->out[0] == '-')
+		++len;
+	if (len > 0)
+	{
+		pad = gen_padding('0', len);
+		tmp = zero_padding(pad, fmt);
+		free(fmt->out);
+		free(pad);
+		fmt->out = tmp;
+	}
+	if (fmt->u_arg.i < 0)
+		return (fmt->out);
+	if (fmt->flags & (1 << 4))
+		tmp = ft_strjoin("+", fmt->out);
+	else if (fmt->flags & (1 << 3))
+		tmp = ft_strjoin(" ", fmt->out);
+	else
+		return (fmt->out);
+	return (free(fmt->out), fmt->out = tmp);
 }
 
 char	*ft_printf_uint(t_format *fmt)
 {
 	char	*str;
+	char	*tmp;
+	int		len;
 
-	str = ft_uitoa(fmt->u_arg.ui);
-	return (str);
+	fmt->out = ft_uitoa(fmt->u_arg.ui);
+	if (fmt->prec > (int)ft_strlen(fmt->out))
+	{
+		len = fmt->prec - ft_strlen(fmt->out);
+		str = malloc(len + 1);
+		str[len] = 0;
+		while (len--)
+			str[len] = '0';
+		tmp = ft_strjoin(str, fmt->out);
+		free(fmt->out);
+		fmt->out = tmp;
+		return (free(str), fmt->out);
+	}
+	return (fmt->out);
 }
 
-int	ft_printf_char(t_format *fmt)
+char	*ft_printf_char(t_format *fmt)
 {
 	unsigned char	c;
 
 	c = (unsigned char)fmt->u_arg.i;
-	return (write(STDOUT_FILENO, &c, 1));
+	fmt->out = malloc(2);
+	if (!fmt->out)
+		return (NULL);
+	fmt->out[0] = c;
+	fmt->out[1] = 0;
+	return (fmt->out);
 }
 
 char	*ft_printf_str(t_format *fmt)
 {
-	char	*str;
-
 	if (!fmt->u_arg.p)
-	{
-		str = ft_strdup("(null)");
-		return (str);
-	}
-	str = ft_strdup((const char *)fmt->u_arg.p);
-	return (str);
+		return (fmt->out = ft_strdup("(null)"));
+	fmt->out = ft_strdup((const char *)fmt->u_arg.p);
+	if (!fmt->out)
+		return (NULL);
+	if (fmt->prec != -1 && fmt->prec < (int)ft_strlen(fmt->out))
+		fmt->out[fmt->prec] = 0;
+	return (fmt->out);
 }
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -37,16 +37,33 @@ int	get_min_width(const char *fmt, int *i)
 
 	width = 0;
 	while (ft_isdigit(fmt[*i]))
-		width = width * 10 + fmt[(*i)++] + '0';
+		width = width * 10 + fmt[(*i)++] - '0';
 	return (width);
 }
 
 int	get_precision(const char *fmt, int *i)
 {
 	int	precision;
+	int	reset;
 
-	precision = -1;
+	precision = 0;
+	reset = 1;
+	if (fmt[(*i) + 1] == '-')
+		reset = 0;
 	while (ft_isdigit(fmt[++(*i)]))
-		precision = precision * 10 + fmt[*i] + '0';
-	return (precision);
+		precision = precision * 10 + fmt[*i] - '0';
+	return (reset * precision);
+}
+
+char	*gen_padding(int c, int n)
+{
+	char	*str;
+
+	str = malloc(n + 1);
+	if (!str)
+		return (NULL);
+	str[n] = 0;
+	while (n--)
+		str[n] = (unsigned char)c;
+	return (str);
 }
