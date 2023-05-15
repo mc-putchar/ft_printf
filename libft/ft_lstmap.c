5452b83605fb1590bf13c8914d97faef646dd0c7
Martin Cutura <mcutura@student.42berlin.de>
2023-05-15 19:48:48 +0200

    Added hardcopy of libft for Moulinette (just in case)

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcutura <mcutura@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/18 18:49:40 by mcutura           #+#    #+#             */
+/*   Updated: 2023/05/05 17:07:29 by mcutura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*map;
+	t_list	*node;
+
+	map = NULL;
+	while (lst)
+	{
+		node = ft_lstnew((*f)(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&map, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&map, node);
+		lst = lst->next;
+	}
+	return (map);
+}
